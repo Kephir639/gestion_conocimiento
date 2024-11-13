5b3129a88241ba97824eae11cb2cc92dd8538f60
Kephir639 <londonoruizcarloseduardo@gmail.com>
2024-11-13 18:33:45 -0500

    Proyecto Investigacion corregido

--- resources/views/modals/proyectos/investigacion/consultarProyectos.blade.php
+++ resources/views/modals/proyectos/investigacion/consultarProyectos.blade.php
@@ -6,21 +6,6 @@
 @endpush
 @section('content')
     <div class="container mt-2">
-
-        @foreach ($controladores as $controlador)
-            @if ($controlador['nombre_controlador'] == 'proyectos_investigacion')
-                @foreach ($controlador['funciones'] as $func)
-                    @if ($func['nombre_funcion'] == 'crear_proyectos_investigacion')
-                        <button title="Registrar Proyectos" id="BtnRegistrarProyecto" class="btn iconoRegistrar p-0"><svg
-                                xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                                <path
-                                    d="M5 21h14a2 2 0 0 0 2-2V5a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2zm2-10h4V7h2v4h4v2h-4v4h-2v-4H7v-2z">
-                                </path>
-                            </svg></button>
-                    @endif
-                @endforeach
-            @endif
-        @endforeach
         <div class="row p-3">
             <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12">
                 <table class="table">
